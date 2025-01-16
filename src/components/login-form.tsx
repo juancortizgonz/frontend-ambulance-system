c1cad4127c4d4af86ac95050a9639ce7bbc0ea8d
Juan Camilo Ortiz <juan.ortiz.gonzalez@correounivalle.edu.co>
2025-01-16 15:15:48 -0500

    Add basic login funcionality

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -2,23 +2,60 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react"
+
+import { useNavigate } from "react-router";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"form">) {
+
+  let navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = { username, password };
+
+    try {
+
+      const response = await fetch("http://localhost:8000/api/v1/auth/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        console.log(data);
+        throw new Error("Error en la solicitud. Es posible que el email o la contraseña sean incorrectos.");
+      }
+
+      const result = await response.json();
+      localStorage.setItem("token", result.token);
+      navigate("/faq");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form className={cn("flex flex-col gap-6", className)} {...props} onSubmit={handleSubmit}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Ingresa al sistema</h1>
         <p className="text-balance text-sm text-muted-foreground">
-          Usa el email y contraseña asignados por el administrador
+          Usa el usuario y contraseña asignados por el administrador
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="micorreo@example.com" required />
+          <Label htmlFor="username">Usuario</Label>
+          <Input id="username" type="text" placeholder="miusuario123" onChange={(e) => setUsername(e.target.value)} required />
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
@@ -30,7 +67,7 @@ export function LoginForm({
               ¿Olvidaste tu contraseña?
             </a>
           </div>
-          <Input id="password" type="password" required />
+          <Input id="password" type="password" onChange={(e) => setPassword(e.target.value)} required />
         </div>
         <Button type="submit" className="w-full">
           Ingresar
