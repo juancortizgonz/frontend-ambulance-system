d33383398db950822ee5a67124c90873be233f77
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2025-03-06 08:04:43 -0500

    Add more fields to the table

--- src/pages/AccidentReportHistory.tsx
+++ src/pages/AccidentReportHistory.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react"
 import { AccidentReport } from "@/types/types"
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table"
 import { downloadPDF, downloadCSV } from "@/utils/exportUtils"
+import { format } from "date-fns"
+import { info } from "console"
 
 const AccidentReportHistory = () => {
     const [data, setData] = useState<AccidentReport[]>(() => [])
@@ -20,12 +22,34 @@ const AccidentReportHistory = () => {
         fetchData();
     }, []);
 
+    console.log(`Data from the API: ${JSON.stringify(data)}`)
+
     const columns = [
         { header: "ID", accessorKey: "id" as keyof AccidentReport },
+        { 
+            header: "Fecha del accidente",
+            accessorKey: "accident_time" as keyof AccidentReport,
+            cell: info => format(new Date(info.row.original.accident_time), "dd/MM/yyyy HH:mm")
+        },
         { header: "Dirección", accessorKey: "address" as keyof AccidentReport },
         { header: "Latitud", accessorKey: "latitude" as keyof AccidentReport },
         { header: "Longitud", accessorKey: "longitude" as keyof AccidentReport },
-        { header: "Severidad", accessorKey: "severity" as keyof AccidentReport }
+        { 
+            header: "¿Está activo?", 
+            accessorKey: "is_active" as keyof AccidentReport,
+            cell: info => info.row.original.is_active ? "Sí" : "No"
+        },
+        { 
+            header: "¿Está resuelto?", 
+            accessorKey: "is_resolved" as keyof AccidentReport,
+            cell: info => info.row.original.is_resolved ? "Sí" : "No"
+        },
+        { header: "Descripción", accessorKey: "description" as keyof AccidentReport },
+        { 
+            header: "Severidad", 
+            accessorKey: "severity" as keyof AccidentReport,
+            cell: info => info.row.original.severity === "BASIC" ? "Básica" : "UCI"
+        }
     ]
 
     const table = useReactTable({
